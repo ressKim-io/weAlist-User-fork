bc898d481f52e6fb6327b97006888e427f5af10e
ressKim <ressk4149@gmail.com>
2025-10-27 23:14:14 +0900

    Jpa db 명확히 변경

--- src/main/java/OrangeCloud/UserRepo/UserRepoApplication.java
+++ src/main/java/OrangeCloud/UserRepo/UserRepoApplication.java
@@ -4,9 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "OrangeCloud.UserRepo.repository")
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class UserRepoApplication {
 	public static void main(String[] args) {
